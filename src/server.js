652996b50a20578ff1c504c91ae0fcf406283429
Marcos Vinicius <viniciusfreitasrj17@gmail.com>
2023-02-14 19:25:09 -0300

    feat: organize routes

--- src/server.js
+++ src/server.js
@@ -1,73 +1,8 @@
 import { createServer } from 'http'
-
-const headers = {"Content-Type": "application/json"}
-
-function notFound(method) {
-  if (method === "GET") {
-    return {
-      status: 404,
-      headers,
-      message: JSON.stringify({ message: "Not Found"}),
-      error: null
-    }
-  }
-}
-
-function success(method) {
-  if (method === "GET") {
-    return {
-      status: 200,
-      headers,
-      message: JSON.stringify({ message: "Success"}),
-      error: null
-    }
-  }
-}
-
-function throwError(...args) {
-  throw new Error('No treated some error')
-}
-
-function throwErrorTreated(...args) {
-  try {
-    throw new Error('Treated some error')
-  } catch (error) {
-    return {
-      status: 500,
-      headers,
-      message: null,
-      error: JSON.stringify({ error: error.message })
-    }
-  }
-}
-
-async function throwErrorPromise(...args) {
-  await Promise.reject('No treated on promise')
-}
-
-async function throwErrorPromiseTreated(...args) {
-  try {
-    await Promise.reject('Treated on promise')
-  } catch (error) {
-    return {
-      status: 500,
-      headers,
-      message: null,
-      error: JSON.stringify({ error })
-    }
-  }
-}
+import { routes, notFound } from './routes.js'
 
 export const server = createServer(async (req, res) => {
   const url = new URL(req.url, `http://${req.headers.host}`)
-
-  const routes = {
-    '/success': success,
-    '/throw-error': throwError,
-    '/throw-error-treated': throwErrorTreated,
-    '/throw-error-promise': throwErrorPromise,
-    '/throw-error-promise-treated': throwErrorPromiseTreated,
-  }
   
   const { 
     status,
